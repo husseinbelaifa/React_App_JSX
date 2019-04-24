663ca92e4ec2b2eccf8358e8ce66e813461ed188
belaifa_el_hussein <belaifahocine@gmail.com>
2019-04-24 15:04:47 +0200

    JSX

--- src/index.js
+++ src/index.js
@@ -1,8 +1,33 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
+const getButtonText = () => {
+  return "click on me!";
+};
+
+const getTime = () => {
+  return new Date().toLocaleTimeString();
+};
+
 const App = () => {
-  return <div>Hi there!</div>;
+  // const buttonText = { text: "click me!" };
+  // const style = { backgroundColor: "blue", color: "white" };
+  // return (
+  //   <div>
+  //     <label className="label" htmlFor="name">
+  //       Enter name:
+  //     </label>
+  //     <input id="name" type="text" />
+  //     <button style={style}>{buttonText.text}</button>
+  //   </div>
+  // );
+
+  return (
+    <div>
+      <div>Current Time:</div>
+      <h3>{getTime()}</h3>
+    </div>
+  );
 };
 
 ReactDOM.render(<App />, document.querySelector("#root"));
